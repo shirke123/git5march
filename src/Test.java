fcb1b4bce14c089dc85bfde725f90d7d584d2172
shirke123 <pujashirke1999@gmail.com>
2022-03-05 12:58:03 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test
+{
+    public  static  void main(String[]args){
+        System.out.println("hii");
+    }
+}
+//https://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@github.com/shirke123/git5march.git
