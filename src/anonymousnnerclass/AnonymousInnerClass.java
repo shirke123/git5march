583b5dfe2b4b18706d4f55b5d70a2c5d44592fbd
shirke123 <pujashirke1999@gmail.com>
2022-03-26 20:56:12 +0530

    commit2

---
+++ src/anonymousnnerclass/AnonymousInnerClass.java
@@ -0,0 +1,20 @@
+package anonymousnnerclass;
+
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+
+//        Add add = (a,b)->a+b; {
+
+        Add  add=new Add(){
+
+            public int addition(int a, int b) {
+                return a + b;
+            }
+        };
+        System.out.println(add.addition(1,6));
+    }
+}
+  interface Add{
+    int addition(int a,int b);
+
+ }
