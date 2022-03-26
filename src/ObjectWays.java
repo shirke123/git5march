b88709a620ef00aeda615f7772cb1afebd4b3e5e
shirke123 <pujashirke1999@gmail.com>
2022-03-26 20:35:24 +0530

    commit

---
+++ src/ObjectWays.java
@@ -0,0 +1,25 @@
+import com.sun.jdi.InvocationException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class ObjectWays {
+
+    public void show()
+    {
+        System.out.println("in show");
+    }
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationException, InvocationTargetException {
+
+        ObjectWays obj = new ObjectWays();
+        obj.show();
+
+        ObjectWays obj1 = ObjectWays.class.newInstance();
+        obj1.show();
+
+        Constructor<ObjectWays> Obj2 = ObjectWays.class.getConstructor();
+        ObjectWays obj3= Obj2.newInstance();
+        obj3.show();
+    }
+}
