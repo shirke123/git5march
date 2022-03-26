1030f67f2d71e1c99f15f4a5bf51a97425b002c1
shirke123 <pujashirke1999@gmail.com>
2022-03-26 20:37:21 +0530

    commit1

---
+++ src/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    public  void eat(){
+        System.out.println("eating");
+    }
+
+
+class Dog extends Animal{
+    public   void bark(){
+        System.out.println("barking");
+    }
+}
+class BabyCat extends Dog{
+    public void weeping(){
+        System.out.println("weeping");
+    }
+}
+
+    public static void main(String[] args) {
+//        BabyCat obj = new BabyCat();
+//        obj.eat();
+//        obj.bark();
+//        obj.weeping();
+    }
+
+    }
---
+++ src/Datatypes.java
@@ -0,0 +1,28 @@
+public class Datatypes {
+    public static void main(String[] args) {
+
+        //primitive data types
+        long l = 131883895835454l;
+        int i = 1360000;
+        byte b = 127;
+        short s = 32000;
+        boolean b1 = true;
+        float f = 98.67f;
+        double d = 166.66d;
+        char ch = 'a';
+
+
+        //non primitive data type
+        Long l1;
+        Integer integer;
+        Short sh;
+        Byte b2;
+        Double d2;
+        Float f2;
+        Boolean b3;
+        Character character;
+
+    }
+}
+
+
